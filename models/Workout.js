77c68ceedcba6c6bf221c6bb19f84ea0654ae969
Xander <xanschwarz@gmail.com>
2021-10-12 15:08:29 -0700

    Workout and index models and tested with npm run seed. No issues currently.

---
+++ models/Workout.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: 'Enter an exercise type.',
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: 'Enter a workout name.',
+      },
+      duration: {
+        type: Number,
+        required: 'Enter an exercise duration, in minutes.',
+      },
+      weight: {
+        type: Number,
+        required: 'Enter the weight used for the exercise.',
+      },
+      reps: {
+        type: Number,
+        required: 'Enter how many reps were performed, per set.',
+      },
+      sets: {
+        type: Number,
+        required: 'Enter how many sets were performed.',
+      },
+      distance: {
+        type: Number,
+        required: 'Enter the distance reached.',
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
+
+module.exports = Workout;
+
+// Test the required's from the schema.
---
+++ models/index.js
@@ -0,0 +1,5 @@
+// Exporting an object containing all of the models. As there is only one model, this file is superfluous but included for consistency with other work.
+
+module.exports = {
+  Workout: require('./Workout'),
+};
