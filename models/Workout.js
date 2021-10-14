e61785d6bf0bf1e8ed348f341571ed9bf83cc16d
Xander <xanschwarz@gmail.com>
2021-10-14 15:39:56 -0700

    Eliminated required from certain exercises fields. This was causing the app to not handle put and post requests as intended.

--- models/Workout.js
+++ models/Workout.js
@@ -1,4 +1,4 @@
-// Test the required's from the schema. Consider eliminating index.js.
+// Consider eliminating index.js.
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
@@ -26,19 +26,15 @@ const workoutSchema = new Schema({
       },
       weight: {
         type: Number,
-        required: 'Enter the weight used for the exercise.',
       },
       reps: {
         type: Number,
-        required: 'Enter how many reps were performed, per set.',
       },
       sets: {
         type: Number,
-        required: 'Enter how many sets were performed.',
       },
       distance: {
         type: Number,
-        required: 'Enter the distance reached.',
       },
     },
   ],
